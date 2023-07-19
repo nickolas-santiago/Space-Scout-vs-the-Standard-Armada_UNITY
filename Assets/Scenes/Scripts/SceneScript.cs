743dbcbda7ff0db569caca83b2a33e8f368d8b0d
nickolas-santiago <knicksantiago@gmail.com>
2023-07-18 22:39:33 -0400

    Health bars are now set in the inspector.
    Update formatting.

--- Assets/Scenes/Scripts/SceneScript.cs
+++ Assets/Scenes/Scripts/SceneScript.cs
@@ -5,45 +5,31 @@ using UnityEngine.UI;
 
 public class SceneScript : MonoBehaviour
 {
+    public List<GameObject> healthbar_object_list = new List<GameObject>();
+    
     public GameObject player_object;
     private GameObject ui_text_score;
     public int current_score;
-    
-    
-    
-    
-    public GameObject healthbar_object;
-    public GameObject healthbarbpanel;
-    public List<GameObject> healthbar_object_list = new List<GameObject>();
-    
-    
     
     // Start is called before the first frame update
     void Start()
     {
         //UI CODE
+        //---health
+        for(int healthbar_object = 0; healthbar_object < GameObject.FindGameObjectsWithTag("UIImageHealthbar").Length; healthbar_object++)
+        {
+            healthbar_object_list.Add(GameObject.FindGameObjectsWithTag("UIImageHealthbar")[healthbar_object]);
+        }
+        for(int healthbar_list_item = 0; healthbar_list_item < healthbar_object_list.Count; healthbar_list_item++)
+        {
+            Debug.Log(healthbar_object_list[healthbar_list_item]);
+            healthbar_object_list[healthbar_list_item].gameObject.SetActive(false);
+        }
+        
         //---score
         current_score = 0;
         ui_text_score = GameObject.FindGameObjectWithTag("UITextScore");
         ui_text_score.GetComponent<Text>().text = current_score.ToString();
-        
-        
-        
-        //---health
-        float ui_panel_healthbar_left_margin = 20.0f;
-        float ui_panel_healthbar_left_spacing = 31.4f;
-        Debug.Log(player_object.GetComponent<PlayerControls>().current_health);
-        for(int health_bar = 0; health_bar < player_object.GetComponent<PlayerControls>().current_health; health_bar++)
-        {
-            float health_bar_xpos = 0;
-            GameObject new_healthbar = Instantiate(healthbar_object) as GameObject;
-            new_healthbar.gameObject.SetActive(true);
-            new_healthbar.transform.SetParent(healthbarbpanel.transform);
-            new_healthbar.GetComponent<RectTransform>().anchoredPosition = new Vector2((ui_panel_healthbar_left_margin + (ui_panel_healthbar_left_spacing * (health_bar))), -20);
-            healthbar_object_list.Add(new_healthbar);
-        }
-        
-        //Debug.Break();
     }
 
     // Update is called once per frame
