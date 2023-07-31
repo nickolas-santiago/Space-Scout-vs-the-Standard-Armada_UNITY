ab669238ee455a6a496309cfe70e66063e074e49
nickolas-santiago <knicksantiago@gmail.com>
2023-07-31 14:32:44 -0400

    Grenades now stop at a desired spot. They don't skip over each other.

--- Assets/Scenes/Scripts/GrenadeScript.cs
+++ Assets/Scenes/Scripts/GrenadeScript.cs
@@ -15,6 +15,9 @@ public class GrenadeScript : MonoBehaviour
     private int time_to_move_in_frames = 60;
     private int time_to_explosion_in_frames = 60;
     
+    private Vector2 endpoint;
+    private Vector2 offset_per_frame;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -26,11 +29,34 @@ public class GrenadeScript : MonoBehaviour
         grenade_rigidbody = GetComponent<Rigidbody2D>();
         //---update the rigidbody's velocity with the caluculated direction and the public force
         //---normalized allows the bullets to travel at the same speed
-        grenade_rigidbody.velocity = new Vector2(direction.x, direction.y).normalized * 1;
+        //grenade_rigidbody.velocity = new Vector2(direction.x, direction.y).normalized * 1;
+        
         
         state_current = "state_moving";
         
-        Debug.Log(time_to_move_in_frames);
+        //Debug.Log(time_to_move_in_frames);
+        
+        Vector2 pos_starting = transform.position;
+        Vector2 nnn = new Vector2(direction.x, direction.y).normalized * 1;
+        Debug.Log(nnn);
+        endpoint = new Vector2((pos_starting.x + nnn.x), (pos_starting.y + nnn.y));
+        Debug.Log(endpoint);
+        offset_per_frame = new Vector2(((endpoint.x - pos_starting.x)/(float)time_to_move_in_frames), ((endpoint.y - pos_starting.y)/time_to_move_in_frames));
+        Debug.Log((pos_starting.x + nnn.x)/60f);
+        Debug.Log((pos_starting.y + nnn.y)/60f);
+        //offset_per_frame = new Vector2((pos_starting.x + nnn.x), (pos_starting.y + nnn.y));
+        /*
+        Debug.Log(offset_per_frame);
+        Debug.Log(offset_per_frame/60);
+        Debug.Log(offset_per_frame/60);
+        */
+        
+        //Vector2 vvv =  new Vector2(1,0);
+        //Vector2 vvv =  new Vector2((pos_starting.x + nnn.x) * 0.60f,0); --- gives a number at least
+        Vector2 vvv =  new Vector2((pos_starting.x + nnn.x)/(float)60.0,0); 
+        //vvv.x = (pos_starting.x + nnn.x)/60f;
+        Debug.Log(vvv);
+        Debug.Log(new Vector2(1,1)/1);
     }
 
     // Update is called once per frame
@@ -40,12 +66,22 @@ public class GrenadeScript : MonoBehaviour
         {
             if(time_to_move_in_frames > 0)
             {
+                transform.position = new Vector2((transform.position.x + offset_per_frame.x), (transform.position.y + offset_per_frame.y));
+                time_to_move_in_frames--;
+            }
+            
+            
+            
+            /*if(time_to_move_in_frames > 0)
+            {
                 time_to_move_in_frames--;
             }
             else
             {
                 grenade_rigidbody.velocity = new Vector2(0,0);
-            }
+            }*/
+            
+            
             /*if(state_current == "state_moving")
             {
                 time_to_move_in_frames--;
